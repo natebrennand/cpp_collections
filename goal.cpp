f13fa7fcf97e4424902a2d961e3b58f09b7edff2
Nate Brennand <natebrennand@gmail.com>
2015-04-03 17:08:59 -0400

    more work

--- goal.cpp
+++ goal.cpp
@@ -11,29 +11,67 @@ public:
         Data = d;
     };
 
-    // template<typename F>
-    // Collection<F> map(F<T> func);
-
-    // template<typename F>
-    // Collection<F> map(std::function<F(T)>);
-    // Collection map(F);
-
-    Collection<T> map(std::function<T(T)> func){
-        std::vector<T> list;
+    template<typename F>
+    Collection<F> map(std::function<F(T)> func) {
+        std::vector<F> list;
         for (int i = 0; i < Data.size(); i++)
             list.push_back(func(Data[i]));
-        return Collection<T>(list);
+        return Collection<F>(list);
     };
+
+    /*
+    template<typename F (typename T)>
+    Collection<F> map(std::function<F(T)> func) {
+        std::vector<F> list;
+        for (int i = 0; i < Data.size(); i++)
+            list.push_back(func(Data[i]));
+        return Collection<F>(list);
+    };
+    */
+
+    //template<typename F>
+    //Collection<F> map(std::function<F(T)> func) {
+    //    std::vector<F> list;
+    //    for (int i = 0; i < Data.size(); i++)
+    //        list.push_back(func(Data[i]));
+    //    return Collection<F>(list);
+    //};
+
+
+    //template<typename X func(typename T)>
+    // Collection<X> map(std::)
 };
 
 
+int increment(int i){
+    return i + 1;
+}
+
+
 int main() {
-    std::vector<int> v = {1, 2, 3, 4};
+    std::vector<int> v = {'a', 'b', 'c', 'd'};
     Collection<int> c = Collection<int>(v);
 
-    auto inc = [](int x) {return x+1;};
-    Collection<int> d = c.map(inc);
+    // auto inc = [](int x) {return x+1;};
+    Collection<int> d = c.map<int>(increment);
     for (int i: d.Data)
         std::cout << i;
+
+
+    auto ascii_from_val = [](int x) {return (char)x;};
+    Collection<char> e = c.map<char>(ascii_from_val);
+    for (auto i: e.Data)
+        std::cout << i << std::endl;
+    std::cout << std::endl;
+
+
+    /*
+    std::vector<char> chars = {'a', 'b', 'c', 'd'};
+    for (int i: chars)
+        std::cout << i;
+    std::cout << std::endl;
+    */
+    Collection<char> z = c.map(ascii_from_val);
+
 }
 
