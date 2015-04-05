1b93c0115d164bc5ac9f189225b2b47038f7578c
ianzapolsky <ianzapolsky@gmail.com>
2015-04-05 17:28:26 -0400

    added std::array constructor and c-style array constructor

--- src/collections.h
+++ src/collections.h
@@ -2,8 +2,10 @@
 #ifndef COLLECTIONS_H
 #define COLLECTIONS_H
 
+#include <array>
 #include <vector>
 #include <list>
+#include <iterator>
 #include <functional>
 #include <iostream>
 #include <utility>
@@ -14,13 +16,27 @@ class Collection {
 private:
     std::vector<T> Data;
 public:
+    // std::vector constructor
     Collection<T>(std::vector<T> d) {
         Data = d;
     }
 
+    // std::list constructor
     Collection<T>(std::list<T> d) {
         for (auto i : d)
             Data.push_back(i);       
+    }
+
+    // std::array constructor
+    template<size_t N>
+    Collection<T>(std::array<T, N> d) {
+        for (auto i : d)
+            Data.push_back(i);       
+    }
+
+    // C-style array constructor (requires length)
+    Collection<T>(T d[], int len) {
+        Data.assign(d, d + len);
     }
 
     Collection<T>
