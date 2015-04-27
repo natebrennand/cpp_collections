83cda861e64ce754b75317125f5c318e707035f8
ianzapolsky <ianzapolsky@gmail.com>
2015-04-27 17:27:48 -0400

    refined define macro syntax in fib example

--- src/syntax/fibonacci.cpp
+++ src/syntax/fibonacci.cpp
@@ -11,9 +11,7 @@ int main() {
     // Collection syntax
 
     def_generator(fibs, int, int prev, int curr) {
-        return Stream<int>(curr, [=]() -> Stream<int> {
-            return fibs(curr, prev + curr);
-        });
+        return Stream<int>(curr, [=]() { return fibs(curr, prev+curr); });
     };
 
     fibs(0, 1).take(10).print();
